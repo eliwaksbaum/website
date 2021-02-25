962a4253beb814b4ba85e78d1b4b2081f96df1b1
eliwaksbaum <62363023+eliwaksbaum@users.noreply.github.com>
2021-02-24 22:02:38 -0500

    complete

--- validate.py
+++ validate.py
@@ -3,16 +3,23 @@ from errors import SiteMapError
 keys = {
     "meta": ["path", "template", "has_subs"],
 
-    "none": ["index_dir"],
-    "blog": ["title", "doc_dir"],
-    "game": ["title", "intro", "width", "height"],
-    "music": ["title", "heading", "intro", "youtube"],
-    "sectionhome": ["title", "heading", "intro"]
+    "templates": {
+        "none": ["index_dir"],
+        "blog": ["title", "doc_dir"],
+        "game": ["title", "intro", "width", "height"],
+        "music": ["title", "heading", "intro", "youtube"],
+        "sectionhome": ["title", "heading", "intro"]
+    },
+    
+    "listings": {
+        "entry": ["summary"],
+        "game": ["summary"]
+    }
 }
 
-def validate(meta, data):
+def validate(meta, data, to_list):
     #Check that the template exists
-    if not (meta["template"] in keys):
+    if not (meta["template"] in keys["templates"]):
         raise SiteMapError(meta["template"] + " is not a defined template @" + meta["path"] + ".")
 
     #Check all the required meta keys are present
@@ -24,11 +31,23 @@ def validate(meta, data):
     if meta["has_subs"]:
         if not ("list_subs" in meta):
             raise SiteMapError("has_subs is true but no value is given for list_subs @" + meta["path"] + ".")
-
+        
     #Check all the required data (template) keys are present
     template = meta["template"]
-    for key in keys[template]:
+    for key in keys["templates"][template]:
         if not (key in data):
             raise SiteMapError("The " + key + " key is missing @" + meta["path"] + ".data")
+
+    #If to_list, this page is generating a listing. We need a valid listing template and the appropriate keys
+    if to_list:
+        if not ("listing" in meta):
+            raise SiteMapError("No listing template is given for the direct sub of a page where list_subs is true @" + meta["path"] + ".")
+        if not (meta["listing"] in keys["listings"]):
+            raise SiteMapError(meta["template"] + " is not a defined listing template @" + meta["path"] + ".")
+        
+        listing = meta["listing"]
+        for key in keys["listings"][listing]:
+            if not (key in data):
+                raise SiteMapError("The " + key + " key is missing @" + meta["path"] + ".data") 
     
     return True
