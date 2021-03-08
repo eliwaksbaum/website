ada1d79be6d11c9f06c035a17ad55aa1caaa20be
eliwaksbaum <62363023+eliwaksbaum@users.noreply.github.com>
2021-03-08 17:03:28 -0500

    dynamic testing

--- .com/cgi-bin/foo.py
+++
@@ -1,3 +0,0 @@
-#C:\Users\Eli\AppData\Local\Programs\Python\Python39
-print("Content-Type: text/html\n")
-print("Hello, World")
--- .com/res/js/tags.js
+++ .com/res/js/tags.js
@@ -2,9 +2,9 @@ function load() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo").innerHTML = this.responseText;
+        document.getElementById("demo").innerHTML = this.responseText + ", yo";
       }
     };
-    xhttp.open("GET", "/cgi-bin/foo.py", true);
+    xhttp.open("GET", "/dynamic?param=value", true);
     xhttp.send();
 }
