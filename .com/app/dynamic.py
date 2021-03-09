466e13a8b15f6a30f2ed77ea1d4d9145cc2b7ccf
eliwaksbaum <62363023+eliwaksbaum@users.noreply.github.com>
2021-03-08 21:12:46 -0500

    dynamic handlers

--- .com/app/dynamic.py
+++ .com/app/dynamic.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
-# test.py
+
+def tagHandler(tag):
+    return "these are the posts with the " + tag + " tag."
+
+def printHandler(txt):
+    return txt
+
+handlers = {
+    "tag": tagHandler,
+    "print": printHandler
+}
+
 def application(env, start_response):
     start_response('200 OK', [('Content-Type','text/html')])
-    return [str(env).encode()] # python3
+
+    path = env["PATH_INFO"].replace("/dynamic/", "", 1)
+    pieces = path.split("/")
+    if pieces[0] in handlers:
+        handler = handlers[pieces[0]]
+        out = handler(pieces[1])
+    else:
+        out = pieces[0] + " doesn't have a handler"
+        
+    return [out.encode()]
