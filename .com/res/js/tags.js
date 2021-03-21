562bf10a7b1b69c5eaa114cfefe045a24ad12714
eliwaksbaum <62363023+eliwaksbaum@users.noreply.github.com>
2021-03-20 20:23:29 -0400

    loadThemedImgs onreadystate

--- .com/res/js/tags.js
+++ .com/res/js/tags.js
@@ -2,7 +2,8 @@ function load() {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo").innerHTML = this.responseText + ", yo";
+        document.getElementById("demo").innerHTML = this.responseText;
+        loadThemedImages(theme_preference);
       }
     };
 
