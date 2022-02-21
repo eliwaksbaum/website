a11821709be50fb8a49d393ded4c99e26de302b6
Eli Waksbaum <eli@waksbaum.com>
2022-02-21 00:14:21 -0500

    one-page prototype

--- .com/res/js/musicplayer.js
+++ .com/res/js/musicplayer.js
@@ -1,41 +1,51 @@
 var canvas; 
 var sheet;
+var music;
 
 var pages;
 var svgPaths;
-var audioPath;
 var numPages;
 var curPage = 0;
 
 function mpInit(json, svgsrcs, audiosrc) {
-    canvas = document.getElementById("player");
     pages = JSON.parse(json);
     svgPaths = svgsrcs;
-    audioPath = audiosrc;
     numPages = pages.length;
+
+    canvas = document.getElementById("player");
+    canvas.style.background = "white";
+    canvas.style.height = "100vh";
+    canvas.style.width = "fit-content";
+    canvas.style.margin = "auto";
 
     sheet = document.createElement("object");
     sheet.setAttribute("data", svgPaths[0]);
     sheet.setAttribute("type", "image/svg+xml");
+    sheet.style.height = "100%";
     canvas.appendChild(sheet);
+
+    music = document.createElement("audio");
+    music.setAttribute("src", audiosrc);
+    canvas.appendChild(music);
+
+    sheet.contentDocument.getElementsByTagName("svg")[0].addEventListener("click", play);
 }
 
-function play(pageIndex) {
-    var pageSVG = document.getElementById("music");
-    var parts = pages[pageIndex];
+function play() {
+    var pageSVG = sheet.contentDocument.getElementsByTagName("svg")[0];
+    var parts = pages[curPage];
     var svgArrays = {"Note": pageSVG.getElementsByClassName("Note"), "Rest": pageSVG.getElementsByClassName("Rest")};
-    var audio = document.getElementById("audio");
 
     for (let i = 0; i < parts.length; i++) {
         MEPlay(parts[i], 0, svgArrays);
     }
-    audio.play();
+    music.play();
 }
 
 function MEPlay(part, pIndex, svgArrays) {
     var measureElement = part[pIndex]
     var svgArray = svgArrays[measureElement["class"]];
-    svgArray[measureElement["index"]].style.fill = "skyblue";
+    svgArray[measureElement["index"]].style.fill = "#0084bf";
 
     playnext = () => {
         svgArray[measureElement["index"]].style.fill = "black";
