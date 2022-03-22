c095986c6403c4c21fa94c7a81a690673063dbe8
Eli Waksbaum <eli@waksbaum.com>
2022-03-21 21:12:25 -0400

    wsplayer display page focus

--- .com/res/js/WebscorePlayer.js
+++ .com/res/js/WebscorePlayer.js
@@ -76,7 +76,7 @@ function WebscoreInit(json, svgsrcs, audiosrc) {
 
     canvas = document.getElementById("player");
     canvas.style.margin = "auto";
-    canvas.addEventListener("click", () => {sheets[cur_page].contentWindow.focus();});
+    canvas.addEventListener("click", () => {sheets[display_page].contentWindow.focus();});
 
     let panel = document.createElement("div");
     panel.innerHTML = panel_HTML;
@@ -187,6 +187,7 @@ function buildPage(data, page_num) {
                     if (is_paused) {
                         let curs = getElementsFromTime(music.currentTime);
                         colorElements(curs, "black");
+                        cur_page = element.page;
                     }
 
                     if (selected != null) {
