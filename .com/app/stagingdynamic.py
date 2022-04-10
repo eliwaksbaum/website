3a0528230b61869a860e7a64faa9c462e6e68ce6
Eli Waksbaum <eli@waksbaum.com>
2022-04-10 19:44:49 -0400

    contact form + POST

--- .com/app/stagingdynamic.py
+++ .com/app/stagingdynamic.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import json
+import json, ssl, smtplib
 
 def getProjs(tag = ""):
     out = ""
@@ -29,10 +29,33 @@ def allHandler(blank):
 def printHandler(txt):
     return txt
 
-handlers = {
+GET_handlers = {
     "tag": tagHandler,
     "print": printHandler,
     "all-projects": allHandler
+}
+
+def mailHandler(body):
+    with open("/home/eli/password.secret", "r") as secret:
+        password = secret.read()
+    password = password[:-1]
+    smtp_server = "smtp.migadu.com"
+    port = 465  # For starttls
+    sender_email = "eli@waksbaum.com"
+    receiver_email = "eli@waksbaum.com"
+    message = "Subject: " + body["subject"] + "\n\n" + body["message"] + "\n\n" + "Sent from : " + body["email"]
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+        server.quit()
+
+    return message
+
+POST_handlers = {
+    "mail": mailHandler
 }
 
 def application(env, start_response):
@@ -40,10 +63,24 @@ def application(env, start_response):
 
     path = env["PATH_INFO"].replace("/dynamic/", "", 1)
     pieces = path.split("/")
-    if pieces[0] in handlers:
-        handler = handlers[pieces[0]]
-        out = handler(pieces[1])
-    else:
-        out = pieces[0] + " doesn't have a handler"
-        
-    return [out.encode()]
+    method = env["REQUEST_METHOD"].upper()
+
+    if (method == "GET"):
+        if pieces[0] in GET_handlers:
+            handler = GET_handlers[pieces[0]]
+            out = handler(pieces[1])
+        else:
+            out = pieces[0] + " doesn't have a GET handler"
+            
+        return [out.encode()]
+
+    elif (method == "POST"):
+        dic = json.load(env['wsgi.input'])
+
+        if pieces[0] in POST_handlers:
+            handler = POST_handlers[pieces[0]]
+            out = handler(dic)
+        else:
+            out = pieces[0] + " doesn't have a POST handler"
+
+        return [out.encode()]
