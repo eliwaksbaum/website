bd29c10f39dea17a3fc4e87d993063e5adc8aabe
Eli Waksbaum <eli.waksbaum@gmail.com>
2023-01-10 12:10:28 -0500

    file_params

--- ssg/src/main.rs
+++ ssg/src/main.rs
@@ -7,7 +7,8 @@ use std::path::{Path, PathBuf, Display};
 struct InsertDef
 {
     path: String,
-    params: Option<Vec<String>>
+    text_params: Option<Vec<String>>,
+    file_params: Option<Vec<String>>
 }
 type InsertCall = HashMap<String, String>;
 
@@ -60,7 +61,7 @@ fn extract_page(raw_page: String, tables: &mut InsertTables, file: &Display, pre
 {
     match (raw_page.find("<insert>"), raw_page.find("</insert>"))
     {
-        (Some(start), Some(end)) => return replace_in_page(&raw_page, start, end, tables, file, prev),
+        (Some(start), Some(end)) => return replace_insert(&raw_page, start, end, tables, file, prev),
         (None, None) => return raw_page,
         _ => {
             println!("Mismatched <insert> tags in {}. Continuing with parse.", file);
@@ -69,9 +70,9 @@ fn extract_page(raw_page: String, tables: &mut InsertTables, file: &Display, pre
     }
 }
 
-fn replace_in_page(page: &str, start: usize, end: usize, tables: &mut InsertTables, file: &Display, prev: Option<&str>) -> String
+fn replace_insert(page: &str, start: usize, end: usize, tables: &mut InsertTables, file: &Display, prev: Option<&str>) -> String
 {
-    let call: InsertCall = toml::from_str(&page[start+8..end]).expect(&format!("Invalid toml in {}.", file));
+    let call: InsertCall = toml::from_str(&page[start+8..end]).expect(&format!("Invalid toml while parsing {}.", file));
 
     let insert_name = call.get("name").expect(&format!("No \"name\" key in an insert tag in {}.", file));
     match prev
@@ -84,43 +85,72 @@ fn replace_in_page(page: &str, start: usize, end: usize, tables: &mut InsertTabl
         }
         None => {}
     }
-    let insert = tables.defs.get(insert_name).expect(&format!("No insert with the name \"{}\" exists.", insert_name));
+
+    let defs = &tables.defs;
+    let insert = defs.get(insert_name).expect(&format!("No insert with the name \"{}\" exists.", insert_name));
 
     let mut page_text = page.to_string();
     
-    let insert_text = tables.texts.entry(insert.path.clone()).or_insert_with(|| read_insert(&insert.path));
+    let insert_text = get_or_put_read(&insert.path, &mut tables.texts);
     let tag_space = &page_text[start..end+9];
-    page_text = page_text.replace(tag_space, insert_text);
+    page_text = page_text.replace(tag_space, &insert_text);
 
-    match &insert.params
+    match &insert.text_params
     {
-        Some(params) => { page_text = replace_in_insert(&page_text, &params, &call, &insert_name, file); },
+        Some(params) => { page_text = replace_text_params(&page_text, &params, &call, &insert_name, file); },
         None => {}    
+    }
+
+    match &insert.file_params
+    {
+        Some(params) => { page_text = replace_file_params(&page_text, &params, &call, &mut tables.texts, &insert_name, file); },
+        None => {}
     }
     
     return extract_page(page_text, tables, file, Some(&insert_name));
 }
 
-fn replace_in_insert(page: &str, params: &Vec<String>, call: &InsertCall, name: &str, file: &Display) -> String
+fn replace_text_params(page: &str, params: &Vec<String>, call: &InsertCall, name: &str, file: &Display) -> String
 {
 
     let mut parsed = page.to_string();
-    
+
     let replacements = params.iter()
         .map(|p| (paramify(p), call.get(p).expect(&format!("An insert tag with the name set to \"{}\" is missing the required \"{}\" parameter in {}.", name, p, file))))
         .collect::<HashMap<String, &String>>();    
 
-    for (param, input) in replacements
+    for (param, arg) in replacements
     {
-        parsed = parsed.replace(&param, input);
+        parsed = parsed.replace(&param, arg);
     }
     return parsed;
+}
+
+fn replace_file_params(page: &str, params: &Vec<String>, call: &InsertCall, path_table: &mut HashMap<String, String>, name: &str, file: &Display) -> String
+{
+    let mut parsed = page.to_string();
+
+    let replacements = params.iter()
+        .map(|p| (paramify(p), call.get(p).expect(&format!("An insert tag with the name set to \"{}\" is missing the required \"{}\" parameter in {}.", name, p, file))))
+        .map(|(param, path)| (param, get_or_put_read(&(String::from("nullaries/") + path), path_table)))
+        .collect::<HashMap<String, String>>();    
+
+    for (param, file_text) in replacements
+    {
+        parsed = parsed.replace(&param, &file_text);
+    }
+    return parsed;
+}
+
+fn get_or_put_read(path: &str, table: &mut HashMap<String, String>) -> String
+{
+    table.entry(path.to_string()).or_insert_with(|| read_insert(&path)).to_string()
 }
 
 fn read_insert(path: &str) -> String
 {
     let insert_path = String::from("inserts/") + path;
-    return fs::read_to_string(&insert_path).expect(&format!("Could not open {}.", path));
+    return fs::read_to_string(&insert_path).expect(&format!("Could not open {}.", &insert_path));
 }
 
 fn paramify(p: &str) -> String
