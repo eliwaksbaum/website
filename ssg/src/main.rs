8e9ac9ffe97255b1a90fa5139ac1f9e8a98e049d
Eli Waksbaum <eli.waksbaum@gmail.com>
2023-01-12 01:27:30 -0500

    the insert is dead! long live the eag!

--- ssg/src/main.rs
+++ ssg/src/main.rs
@@ -19,23 +19,49 @@ fn main()
 {
     let table: EagTable = toml::from_str(&fs::read_to_string("eags/eags.toml").unwrap()).unwrap();
     let mut cache = FileCache::new();
-    let html = generate_page(&PathBuf::from("docs/foo.heml"), &table, &mut cache);
-    fs::write(PathBuf::from("./foo.html"), html).expect("yikes");
+    
+    let read_base = PathBuf::from("docs/");
+    let write_base = PathBuf::from("../.com/");
+
+    build(&read_base, &read_base, &write_base, &table, &mut cache);
+}
+
+fn build(dir: &Path, read_base: &Path, write_base: &Path, table: &EagTable, cache: &mut FileCache)
+{
+    let files = fs::read_dir(dir).unwrap_or_else(|_| panic!("Could not open {:?}.", &dir));
+    for entry in files
+    {
+        let path = entry.expect("Error in the DirEntry iterator while building.").path();
+        let write = write_base.join(path.strip_prefix(read_base).unwrap());
+        if path.is_dir()
+        {
+            if !write.exists()
+            {
+                fs::create_dir(&write).unwrap_or_else(|_| panic!("Could not create dir {}", write.display()));
+            }
+            build(&path, read_base, &write_base, table, cache);
+        }
+        else
+        {
+            let file_write = write.with_extension("html");
+            fs::write(&file_write, generate_page(&path, table, cache)).unwrap_or_else(|_| panic!("Could not write to {:?}", &file_write));
+        }
+    }
 }
 
 fn generate_page(read_path: &Path, table: &EagTable, cache: &mut FileCache) -> String
 {
     let heml = fs::read_to_string(read_path).unwrap_or_else(|_| panic!("Could not open {:?}.", &read_path));
     
-    if &heml[0..2] != "<<"
-    {
-        panic!("Failed to parse {:?}. HEML files must start with a double-angle-bracketed eag.", read_path);
-    }
+    // if &heml[0..2] != "<<"
+    // {
+    //     panic!("Failed to parse {:?}. HEML files must start with a double-angle-bracketed eag.", read_path);
+    // }
 
     parse(heml, table, cache).unwrap_or_else(|m| panic!("Parsing of {:?} failed while {}", read_path, m))
 }
 
-fn parse(heml: String, table: &EagTable, cache: &mut FileCache) -> Result<String, String>
+fn parse(mut heml: String, table: &EagTable, cache: &mut FileCache) -> Result<String, String>
 {
     let open_open = match heml.find("<<") {
         Some(i) => i,
@@ -50,7 +76,7 @@ fn parse(heml: String, table: &EagTable, cache: &mut FileCache) -> Result<String
     let eag_name = &heml[(open_open + 2)..(open_close)];
 
     let toml_end = match (&heml[(open_close + 2)..]).find("<") {
-        Some(i) => i,
+        Some(i) => i + open_close + 2,
         None => { return Err(String::from("searching for a \"<\" to mark the end of <<") + eag_name + ">>'s arguments.") }
     };
 
@@ -59,7 +85,8 @@ fn parse(heml: String, table: &EagTable, cache: &mut FileCache) -> Result<String
         None => { return Err(String::from("searching for \"<</") + eag_name + ">>\" to close the section.") }
     };
 
-    println!("{}", &heml[(open_close + 2)..toml_end]);
+    let close_close = close_open + eag_name.len() + 5;
+
     let args: EagArgs = match toml::from_str(&heml[(open_close + 2)..toml_end]) {
         Ok(a) => a,
         Err(e) => { return Err(String::from("Parsing <<") + eag_name + ">>'s arguments. Here's the toml error: " + &e.to_string()) }
@@ -77,9 +104,11 @@ fn parse(heml: String, table: &EagTable, cache: &mut FileCache) -> Result<String
 
     let eag_doc = read_eag_doc(&eag.path);
 
-    let next = replace(&eag_doc, &rlz);
+    let replacement = replace(&eag_doc, &rlz);
 
-    parse(next, table, cache)
+    heml.replace_range(open_open..close_close, &replacement);
+
+    parse(heml, table, cache)
 }
 
 fn generate_eag_realization<'a>(eag: &'a Eag, args: &EagArgs, inside_text: &str, cache: &mut FileCache) -> Result<EagRealization, &'a str>
