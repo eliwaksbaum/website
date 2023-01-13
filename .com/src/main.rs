97bb9f407f8418babeab2cdb98d90c07109ac091
Eli Waksbaum <eli.waksbaum@gmail.com>
2023-01-13 17:06:01 -0500

    some paths

--- .com/src/main.rs
+++ .com/src/main.rs
@@ -3,17 +3,17 @@ use rocket::fs::{ FileServer, NamedFile };
 
 #[catch(404)]
 async fn not_found() -> Option<NamedFile> {
-    NamedFile::open(".com/404.html").await.ok()
+    NamedFile::open("404.html").await.ok()
 }
 
 #[catch(403)]
 async fn permission_denied() -> Option<NamedFile> {
-    NamedFile::open(".com/403.html").await.ok()
+    NamedFile::open("403.html").await.ok()
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-        .mount("/", FileServer::from(".com/public/"))
+        .mount("/", FileServer::from("public/"))
         .register("/", catchers![not_found, permission_denied])
 }
