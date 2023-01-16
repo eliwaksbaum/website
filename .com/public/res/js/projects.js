a4c41e86ffb891a2a5b9df66abff6f5a670358bf
Eli Waksbaum <eli.waksbaum@gmail.com>
2023-01-16 18:24:04 -0500

    getting somewhere with the drawers

--- .com/public/res/js/projects.js
+++ .com/public/res/js/projects.js
@@ -1,13 +1,42 @@
+let cur_open = null;
+let holder = document.getElementById("holder");
 
-function openDrawer(id)
+window.addEventListener("resize", (e) => { sizeDrawer(); })
+
+function toggleDrawer(card, drawer_id)
 {
-    var drawer = document.getElementById(id);
+    let drawer = document.getElementById(drawer_id);
     if (drawer.style.display == "block")
     {
         drawer.style.display = "none";
+        cur_open = null;
     }
     else
     {
         drawer.style.display = "block";
+        if (cur_open != null)
+        { 
+            cur_open.style.display = "none";
+        }
+        cur_open = drawer;
+        sizeDrawer();
+    }
+}
+
+function sizeDrawer()
+{
+    if (cur_open != null)
+    {   
+        let cur_listing = cur_open.parentElement;
+
+        let holder_rect = holder.getBoundingClientRect();
+        let listing_rect = cur_listing.getBoundingClientRect();
+        let card_rect = cur_open.previousElementSibling.getBoundingClientRect();
+
+        let scale = (holder_rect.width / (listing_rect.width));
+        cur_open.style.width = `${scale * 100}%`;
+        cur_open.style.right = `${.98 * (listing_rect.left - holder_rect.left)}px`;
+        //cur_open.style.left = `${.98 * (holder_rect.right - (listing_rect.left + (card_rect.width * scale)))}px`;
+        console.log(holder_rect.right,  listing_rect.left + (card_rect.width * scale));
     }
 }
