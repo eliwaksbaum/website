16905261c895f18a58400dded1340ef9636b7022
Eli Waksbaum <eli.waksbaum@gmail.com>
2023-01-17 11:39:56 -0500

    and_then! i'm so good at rust

--- .com/src/main.rs
+++ .com/src/main.rs
@@ -2,45 +2,46 @@
 use rocket::fs::{ FileServer, NamedFile };
 use rocket::response::content::RawHtml;
 use std::fs;
-use rand::seq::SliceRandom;
-use rand::thread_rng;
+use std::io::{ Error, ErrorKind };
+use rand::{ seq::SliceRandom, thread_rng };
 
 #[catch(404)]
-async fn not_found() -> Option<NamedFile> {
+async fn not_found() -> Option<NamedFile>
+{
     NamedFile::open("404.html").await.ok()
 }
 
 #[catch(403)]
-async fn permission_denied() -> Option<NamedFile> {
+async fn permission_denied() -> Option<NamedFile>
+{
     NamedFile::open("403.html").await.ok()
 }
 
 #[get("/projects")]
-fn shuffle_cards() -> Option<RawHtml<String>> {
-    let file = fs::read_to_string("public/projects/index.html");
-    match file
+fn shuffle_cards() -> Result<RawHtml<String>, Error>
     {
-        Err(_) => None,
-        Ok(mut html) => {
+    fs::read_to_string("public/projects/index.html").and_then(|mut html|
+    {
             let count = html.matches("order:0").count();
     
             for n in get_order(count)
             {
                 html = html.replacen("order:0", &(String::from("order:") + &n.to_string()), 1);
             }
-            Some(RawHtml(html))
-        }
-    }
+        Ok(RawHtml(html))
+    })
 }
 
-fn get_order(n: usize) -> Vec<usize> {
+fn get_order(n: usize) -> Vec<usize>
+{
     let mut ordered: Vec<usize> = (0..=n).collect();
     ordered.shuffle(&mut thread_rng());
     ordered
 }
 
 #[launch]
-fn rocket() -> _ {
+fn rocket() -> _
+{
     rocket::build()
         .mount("/", FileServer::from("public/"))
         .mount("/", routes![shuffle_cards])
