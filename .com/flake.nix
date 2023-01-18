5853165322548658cfbb6a1f25ed4f6093f236ff
Eli Waksbaum <eli.waksbaum@gmail.com>
2023-01-18 16:09:17 -0500

    whoops misplaced that flake file

---
+++ .com/flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "Rocket Server";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , rust-overlay
+    , crane
+    , ...
+    } @ inputs:
+    flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ] (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      inherit (pkgs) lib;
+
+      rust = rust-overlay.packages.${system}.default.override {
+        extensions = [ "rust-src" ];
+      };
+      craneLib = (crane.mkLib pkgs).overrideToolchain rust;
+
+      rocket = craneLib.buildPackage {
+        src = self;
+      };
+    in
+    {
+      packages.rocket = rocket;
+      defaultPackage = rocket;
+    });
+}
