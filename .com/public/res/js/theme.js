5624238216992b7e62908ce192bb02c987101197
Eli Waksbaum <eli.waksbaum@gmail.com>
2023-01-24 13:28:57 -0500

    theme

--- .com/public/res/js/theme.js
+++ .com/public/res/js/theme.js
@@ -1,64 +1,20 @@
-var css;
-var desktop_button;
-var mobile_button;
-
-
 var colors = ["rgb(146, 101, 243)", "rgb(236, 79, 19)", "rgb(77, 196, 295)", "rgb(19, 236, 205)", "rgb(255, 77, 255)", "rgb(255, 136, 77)", "rgb(0, 213, 0)"]
 
-var theme_preference = localStorage.getItem("theme");
-if (theme_preference == null) {
-    theme_preference = "light";
-}
-
-var root = document.documentElement;
-var style = getComputedStyle(root);
-var dark = style.getPropertyValue("--dark");
-var light = style.getPropertyValue("--light");
-
-function setCss(theme) {
-    css.href = "/res/stylesheets/" + theme + ".css";
-}
-
-function setButton(theme) {
-    var anti;
-    if (theme == "light") {
-        anti = "dark";
-    }
-    else {
-        anti = "light";
-    }
-    desktop_button.src = "/res/svg/" + anti + ".svg";
-    desktop_button.alt = anti;
-    mobile_button.src = "/res/svg/" + anti + ".svg";
-    mobile_button.alt = anti;
-}
-
-function setTheme(theme) {
-    setButton(theme);
-    setCss(theme);
-    loadThemedImages(theme);
-}
-
 function toggleTheme() {
-    if (css.getAttribute("href") == "/res/stylesheets/light.css") {
-        setTheme("dark");
-        localStorage.setItem("theme", "dark");
-    }
-    else {
-        setTheme("light");
-        localStorage.setItem("theme", "light");
+    toggleCss();
+    toggleThemedImages();
+}
+
+function toggleThemedImages() {
+    var imgs = document.getElementsByClassName("themed");
+    for (var i = 0; i < imgs.length; i++) {
+        imgs[i].src = imgs[i].src.split("?a=")[0].replace("cur", "toggle") + "?a=" + performance.now();
     }
 }
 
-function findCss() {
-    css = document.getElementById("theme");
-    setCss(theme_preference);
-}
-
-function findButton() {
-    desktop_button = document.getElementById("theme-button-desktop");
-    mobile_button = document.getElementById("theme-button-mobile");
-    setButton(theme_preference);
+function toggleCss() {
+    let theme = document.getElementById("theme");
+    theme.href = theme.href.split("?a=")[0].replace("cur", "toggle") + "?a=" + performance.now();
 }
 
 function colorize() {
@@ -68,14 +24,5 @@ function colorize() {
     
     for (var i = 0; i < headers.length; i++) {
         headers[i].style.backgroundColor = color;
-    }
-}
-
-function loadThemedImages(theme) {
-    var imgs = document.getElementsByClassName("themed");
-    for (var i = 0; i < imgs.length; i++) {
-        var themedSrc = imgs[i].getAttribute("data-themedSrc");
-        var src = themedSrc.replace("theme", theme);
-        imgs[i].src = src;
     }
 }
